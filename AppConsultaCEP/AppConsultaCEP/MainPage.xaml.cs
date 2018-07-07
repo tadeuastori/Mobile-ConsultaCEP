5320a04d0b021fba9c52986fe017d2d355b362d4
Carlos Tadeu Galeri Astori <tadeu.astori@hotmail.com>
2018-07-07 14:36:01 -0300

    Add project files.

---
+++ AppConsultaCEP/AppConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using AppConsultaCEP.Servicos;
+using AppConsultaCEP.Servicos.Modelo;
+using System;
+using Xamarin.Forms;
+
+namespace AppConsultaCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.IsEnabled = true;
+            CEP.IsEnabled = true;
+            RESULTADO.IsVisible = false;
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs e)
+        {
+            string cep = (!string.IsNullOrEmpty(CEP.Text) ? CEP.Text.Trim() : string.Empty);
+            string espaco = Environment.NewLine;
+            string resultado = string.Empty;
+
+            if (IsValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if (end == null)
+                    {
+                        DisplayAlert("Erro", $"CEP {cep} não encontrado!", "OK");
+                    }
+                    else
+                    {
+                        resultado += $@" CEP: {end.cep}{espaco}";
+                        resultado += $@" {end.logradouro}{espaco}";
+                        resultado += $@" Bairro: {end.bairro}{espaco}";
+                        resultado += $@" Cidade: {end.localidade}{espaco}";
+                        resultado += $@" UF: {end.uf}{espaco}";
+                        resultado += $@" IBGE: {end.ibge}";
+
+                        RESULTADO.IsVisible = true;
+                        RESULTADO.Text = resultado;
+                        CEP.Text = string.Empty;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Erro", ex.Message, "OK");
+                }
+            }
+        }
+
+        private bool IsValidCEP(string cep)
+        {
+            if (cep.Length < 8)
+            {
+                DisplayAlert("ERRO", "O CEP deve ter 8 dígitos.", "OK");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AppConsultaCEP/AppConsultaCEP/Servicos/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using AppConsultaCEP.Servicos.Modelo;
+using Newtonsoft.Json;
+
+namespace AppConsultaCEP.Servicos
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+        }
+    }
+}
